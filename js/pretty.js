f0cdcfec0748ee8e367095512b4b22ff29d2edce
Jayden <jayden.gilmore@centre.edu>
2020-11-12 22:10:45 -0500

    MAde graph

--- js/pretty.js
+++ js/pretty.js
@@ -70,7 +70,20 @@ var makeTranslateString = function(x,y)
 var drawAxes = function(graphDim,margins,
                          xScale,yScale)
 {
-   
+   var xAxis = d3.axisBottom(xScale);
+   var yAxis = d3.axisLeft(yScale);
+    
+    var axes = d3.select("svg")
+    .append("g");
+    axes.append("g")
+    .attr("transform", "translate("+margins.left+","+(margins.top+graphDim.height)+")")
+    .call(xAxis);
+    
+    axes.append("g")
+    .attr("transform", "translate("+margins.left+","+(margins.top)+")")
+    .call(yAxis);
+    
+    
  
 }
 
@@ -79,15 +92,38 @@ var drawAxes = function(graphDim,margins,
 //margins - objedct that stores the size of the margins
 var drawLabels = function(graphDim,margins)
 {
+    var labels = d3.select("svg")
+    .append("g")
+    .classed("labels", true);
     
+    labels.append("text")
+    .text("Trump Support")
+    .classed("title", true)
+    .attr("text-anchor", "middle")
+    .attr("x", margins.left+(graphDim.width/2))
+    .attr("y", margins.top)
+    
+    labels.append("text")
+    .text("Percent White")
+    .classed("label", true)
+    .attr("text-anchor", "middle")
+    .attr("x", margins.left+graphDim.width/2)
+    .attr("y", graphDim.height+
+          (margins.bottom+margins.top))
+    
+    labels.append("g")
+    .attr("transform", "translate(20,"+(margins.top+
+    (graphDim.height/2))+")")
+    .append("text")
+    .text("Percent Voting for Trump")
+    .classed("label", true)
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(90)")
 }
 
 
 var drawLegend = function(graphDim,margins)
-{
-    
- 
-   var categories = [
+{ var categories = [
        {
            class:"lessCollege",
            name:"Less College"
@@ -98,19 +134,45 @@ var drawLegend = function(graphDim,margins)
        }
     ]
 
+    var legend = d3.select("svg")
+    .append("g")
+    .classed("legend", true)
+    .attr("transform", "translate("+
+          (margins.left+10)+","+(margins.top+10)+")");
+ 
+ var entries = legend.selectAll("g")
+ .data(categories)
+ .enter()
+ .append("g")
+ .classed("legendEntry", true)
+ .attr("class", function(category){
+     return category.class;
+ })
+ .attr("trasform", function(categories, index){
+     return "translate(0,"+index*20+")";
+ })
+ 
+ entries.append("rect")
+ .attr("width", 10)
+ .attr("height", 10)
+ 
+ entries.append("text")
+ .text(function(category){return category.name;})
+ .attr("x", 15)
+ .attr("y", 10)
 
-    
-    
-    
+
+
+
 }
 
 //sets up several important variables and calls the functions for the visualization.
 var initGraph = function(counties)
 {
     //size of screen
-    var screen = {width:800,height:600}
+    var screen = {width:700,height:600}
     //how much space on each side
-    var margins = {left:30,right:20,top:20,bottom:30}
+    var margins = {left:70,right:30,top:50,bottom:50}
     
     
     
